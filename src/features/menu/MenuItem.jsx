5245f83c339c0ea212b13587c26c20ba28952095
seanka <and.seanka@gmail.com>
2024-01-04 09:51:47 +0700

    Added data laoding in react router for menu screen

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,15 +1,15 @@
+import { formatCurrency } from "../../utils/helpers";
+
 function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
     <li>
       <img src={imageUrl} alt={name} />
       <div>
         <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
-        </div>
+        <p>{ingredients.join(", ")}</p>
+        <div>{!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}</div>
       </div>
     </li>
   );
