f6a5cf4b9c73de5465ea90ba408b5919b812a21c
seanka <and.seanka@gmail.com>
2024-01-04 14:18:44 +0700

    Configured web layout

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold"></div>;
+  return <div className="hidden text-sm font-semibold md:block"></div>;
 }
 
 export default Username;
